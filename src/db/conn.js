abfe37f853e1cab2804b7356a93c1ae4df337fd1
Ravi Prakash <ravip140891@gmail.com>
2022-12-30 12:23:48 +0530

    Link modified

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,7 +2,8 @@ const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
 
-const DB = process.env.SECRET_Key;
+const DB =
+  "mongodb+srv://Ravi1408:hJsKwVDCc9OQQbaI@cluster0.rg3mox3.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.set("strictQuery", true);
 mongoose
